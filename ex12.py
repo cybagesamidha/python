0f321b5ea5ceb67e09afc7174ce3c20bd192058a
cybagesamidha <dubey.samidha755@gmail.com>
2018-02-09 14:10:54 +0530

    adding new files

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = raw_input ("how old are you?")
+height = raw_input ("how many tall are you?")
+weight = raw_input("how much do you weight?")
+
+print "So, you are %r old, %r tall and %r heavy." % (age, height, weight)
---
+++ ex9.py
@@ -0,0 +1,11 @@
+days = " Mon Tue Wed Thu Fri Sat Sun"
+months = " Jan\nFeb\nmar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print "Here's the days: ", days
+print "Here's the months: ", months
+
+print """
+there is somthing going on here.
+with the tree doble-quotes.
+we'll be able to type as much as we like.
+"""
